d20071c7e2ddfb544b0f68cf8614c5cfa369c6fb
Ramón Vázquez Pérez-Batallón <ravapeba@protonmail.com>
2021-02-09 09:22:17 +0100

    Add files via upload

--- r/funciones1.R
+++ r/funciones1.R
@@ -10,17 +10,26 @@ get_datos <- function(file,skip) {
   invisible(datos)
 }
 
-plot_datos <- function(datos,tit,subtit,ejex,ejey,caption){
+plot_datos <- function(datos,tit,subtit,caption){
 plot <- ggplot(datos,aes(x = Year, y = Tanom, colour=y))+
   geom_line(colour="#FF9999")+theme_bw(base_size=14)+
   labs(title=tit,
        subtitle=subtit,
-	 x=ejex,
-       y=ejey,
        caption=caption)
 return(plot)
 }
 
+#### decora grafico ggplot
+decora1 <- function(plot,color,fontsize,xbreaks,ybreaks,xlab,ylab,hline){
+plot <- plot +
+geom_line(colour=color,size=0.5)+theme_bw(base_size=fontsize)+
+scale_x_continuous(breaks = xbreaks)+
+scale_y_continuous(breaks = ybreaks)+
+xlab(xlab)+ylab(ylab)+
+geom_hline(yintercept=hline, size=0.5, linetype="dashed", color = "grey", alpha=0.8)
+
+return(plot)
+}
 
 ### quitar un paquete
 detach_package <- function(pkg, character.only = FALSE)
